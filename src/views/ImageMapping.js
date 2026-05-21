a65c5126acb52cdd51877fde2a2b6abe4203541f
Raditya Narendra Hadi <blankkuhaku73@gmail.com>
2026-05-21 11:21:51 +0700

    Update gambar flashcard pernapasan

--- src/views/ImageMapping.js
+++ src/views/ImageMapping.js
@@ -16,4 +16,20 @@ export const OrganImageFlash = {
     'Pericardium': require('../../assets/sistem_peredaran_darah/selaput-pembungkus-jantung.png'),
     'Myocardium': require('../../assets/sistem_peredaran_darah/lapisan-otot-jantung.png'),
 
+    // --- SYSTEM RESPIRATORY ---
+    'Cavum Nasalis': require('../../assets/sistem_pernapasan/rongga-hidung.png'),
+    'Laring': require('../../assets/sistem_pernapasan/pangkal-tenggorokan.png'),
+    'Trakea': require('../../assets/sistem_pernapasan/batang tenggorokan.png'),
+    'Bronkus': require('../../assets/sistem_pernapasan/cabang tenggorokan.png'),
+    'Bronkiolus': require('../../assets/sistem_pernapasan/ranting tenggorokan.png'),
+    'Alveolus': require('../../assets/sistem_pernapasan/alveolus.png'),
+    'Epiglottis': require('../../assets/sistem_pernapasan/epiglottis.png'),
+    'Plica Vocalis': require('../../assets/sistem_pernapasan/pita-suara.png'),
+    'Pleura': require('../../assets/sistem_pernapasan/selaput_paru.png'),
+    'Diaphragma': require('../../assets/sistem_pernapasan/diaphragma.png'),
+    'Sinus Paranasal': require('../../assets/sistem_pernapasan/sinus-paranasal.png'),
+    'Faring': require('../../assets/sistem_pernapasan/faring.png'),
+    'Nasofaring': require('../../assets/sistem_pernapasan/nasofaring.png'),
+
+    // --- SYSTEM DIGESTIVE ---
 };
